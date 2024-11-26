99f47e1fb283548c63b8fc7e7c03844e3709856e
AdrianC2095 <adriancuenca095@gmail.com>
2024-11-25 20:37:57 -0500

    Creacion del proyecto en NetBeans

---
+++ ProyectoBimestral1/src/AdivinarNumeroMejorado2_0.java
@@ -0,0 +1,17 @@
+/***
+ * Se le solicita desarrolar un demo tipo juego, que permita generar un número
+ * aleatorio y le rete al ususario que lo adivine. Si no adivina el número,
+ * el juego debe sugerirle lo intentenuevamente con pistas (es mayor, menor, 
+ * es par, impar, es primo, pertenece a la serie de fibonacy, es multiplo de N, etc)
+ * el ususario es quien deberá ir pidiendo repetitivamente que el sistema le muestre o no,
+ * más pistas, solo que, si el usuario pide más pistas el sistema automáticamente
+ * ñe resta un intento y le informa cuantos intentos le quedan. Al finalizar el juego, 
+ * se debe felicitar al usuario si gana y como, o pedir que lo intente de nuevo.
+ * @author Adrián Cuenca
+ */
+import java.util.Scanner;
+public class AdivinarNumeroMejorado2_0 {
+    public static void main(String[] args) {
+        Scanner tcl = new Scanner(System.in);
+    }
+}
