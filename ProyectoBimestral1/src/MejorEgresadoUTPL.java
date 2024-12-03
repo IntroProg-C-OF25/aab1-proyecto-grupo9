514d806e62e31e56478172ebdea986d1671b2b4a
CalvitaJuanJo <juanjosecalva427@gmail.com>
2024-12-03 16:31:55 -0500

    Finalizo El Mejor Egresado

--- ProyectoBimestral1/src/MejorEgresadoUTPL.java
+++ ProyectoBimestral1/src/MejorEgresadoUTPL.java
@@ -1,4 +1,25 @@
-
+/***
+ * La UTPL, cada fin de periodo realiza las ceremonias de incorporación de sus
+ * nuevos profesionales de todas las carreras. En dicha ceremonia de entre todos
+ * los puntos desarrollados en la agenda, uno de los más relevantes es el
+ * discurso del mejor egresado de la promoción. Para elegirlo, las autoridades de
+ * entre algunos aspectos, considera su rendimiento académico durante toda la
+ * carrera, es aquí en donde la UTPL le contrata con el objetivo de colaborar en la
+ * automatización de este proceso de cálculo y generación de resultados
+ * académicos dados los siguientes lineamientos:
+ - Autogenere los estudiantes con los datos/atributos necesarios (lo básico es
+  su nombre y promedio final) con el fin de realizar la comparativa de su
+  rendimiento académico entre todos los estudiantes a graduarse y así poder
+  elegir al mejor egresado con el mayor promedio.
+ - Realizar los cálculos y/o comparativas para filtrar al estudiante con el mayor
+  promedio de egreso.
+ - Al finalizar dicho proceso, como resultado Ud. debe mostrar los datos
+  completos del mejor egresado, y el por qué fue elegido de entre todo el
+  grupo.
+ - Considerar la excepción/probabilidad de que puedan existir máximo 2
+  mejores egresados, dado el caso, usted deberá mostrar los dos mejores
+  egresados.
+ */
 import java.util.Scanner;
 
 public class MejorEgresadoUTPL {
